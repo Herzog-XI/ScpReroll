23d9c9fa74233f1e8e562ecd1310e17719100f4a
Herzog-XI <egmherzog@gmail.com>
2026-07-04 16:56:25 +0200

    Update Plugin.cs

--- ScpReroll/Plugin.cs
+++ ScpReroll/Plugin.cs
@@ -1,1 +1,41 @@
+using System;
+using Exiled.API.Features;
 
+namespace ScpReroll
+{
+    public class Plugin : Plugin<Config>
+    {
+        public static Plugin Instance { get; private set; }
+
+        public override string Name => "ScpReroll";
+        public override string Author => "Oliver";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(9, 0, 0);
+
+        public EventHandlers EventHandlers { get; private set; }
+        public RerollManager RerollManager { get; private set; }
+
+        public override void OnEnabled()
+        {
+            Instance = this;
+
+            RerollManager = new RerollManager();
+            EventHandlers = new EventHandlers();
+
+            EventHandlers.Register();
+
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            EventHandlers?.Unregister();
+
+            EventHandlers = null;
+            RerollManager = null;
+            Instance = null;
+
+            base.OnDisabled();
+        }
+    }
+}
