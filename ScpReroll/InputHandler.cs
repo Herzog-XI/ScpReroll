85f0581468c76f35032f3e74a8a7231d3bd33434
Herzog-XI <egmherzog@gmail.com>
2026-07-04 16:59:11 +0200

    Update InputHandler.cs

--- ScpReroll/InputHandler.cs
+++ ScpReroll/InputHandler.cs
@@ -1,1 +1,20 @@
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
 
+namespace ScpReroll
+{
+    public class InputHandler
+    {
+        public void OnUsingItem(UsingItemEventArgs ev)
+        {
+            // Temporary fallback input:
+            // Pressing H directly may require a Harmony/input patch.
+            // For now, reroll will be triggered through the client command in the next step.
+        }
+
+        public static void TryActivate(Player player)
+        {
+            Plugin.Instance.RerollManager.TryReroll(player);
+        }
+    }
+}
