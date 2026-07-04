cf02b218de0a8bc05db4363919cbc2856014cf95
Herzog-XI <egmherzog@gmail.com>
2026-07-04 16:56:49 +0200

    Update EventHandlers.cs

--- ScpReroll/EventHandlers.cs
+++ ScpReroll/EventHandlers.cs
@@ -1,1 +1,43 @@
+using Exiled.Events.Handlers;
 
+namespace ScpReroll
+{
+    public class EventHandlers
+    {
+        public void Register()
+        {
+            Player.Spawned += OnPlayerSpawned;
+            Player.Dying += OnPlayerDying;
+            Server.RoundStarted += OnRoundStarted;
+            Server.RestartingRound += OnRoundRestarting;
+        }
+
+        public void Unregister()
+        {
+            Player.Spawned -= OnPlayerSpawned;
+            Player.Dying -= OnPlayerDying;
+            Server.RoundStarted -= OnRoundStarted;
+            Server.RestartingRound -= OnRoundRestarting;
+        }
+
+        private void OnPlayerSpawned(Exiled.Events.EventArgs.Player.SpawnedEventArgs ev)
+        {
+            Plugin.Instance.RerollManager.OnSpawn(ev.Player);
+        }
+
+        private void OnPlayerDying(Exiled.Events.EventArgs.Player.DyingEventArgs ev)
+        {
+            Plugin.Instance.RerollManager.OnDeath(ev.Player);
+        }
+
+        private void OnRoundStarted()
+        {
+            Plugin.Instance.RerollManager.OnRoundStarted();
+        }
+
+        private void OnRoundRestarting()
+        {
+            Plugin.Instance.RerollManager.OnRoundEnded();
+        }
+    }
+}
