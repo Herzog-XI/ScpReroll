f53cfd52bfa5f803a295b6a32d5554c79d239c3f
Herzog-XI <egmherzog@gmail.com>
2026-07-04 16:38:47 +0200

    Update Config.cs

--- ScpReroll/Config.cs
+++ ScpReroll/Config.cs
@@ -1,1 +1,16 @@
+using Exiled.API.Interfaces;
 
+namespace ScpReroll
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+
+        public int RerollTime { get; set; } = 10;
+
+        public string Keybind { get; set; } = "RightControl";
+
+        public bool ShowCountdown { get; set; } = true;
+    }
+}
