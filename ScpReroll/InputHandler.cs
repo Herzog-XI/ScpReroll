7fc36de60c6731ff7cad52ca99bcbe92c5e58885
Herzog-XI <egmherzog@gmail.com>
2026-07-04 17:42:26 +0200

    Update InputHandler.cs

--- ScpReroll/InputHandler.cs
+++ ScpReroll/InputHandler.cs
@@ -4,20 +4,18 @@ using Exiled.API.Features;
 
 namespace ScpReroll
 {
-    public static class InputHandler
-    {
-        public static void TryActivate(Player player)
-        {
-            Plugin.Instance.RerollManager.TryReroll(player);
-        }
-    }
-
     [CommandHandler(typeof(ClientCommandHandler))]
     public class RerollCommand : ICommand
     {
-        public string Command => "reroll";
-        public string[] Aliases => new[] { "rerollscp", "scpreroll" };
-        public string Description => "Reroll your SCP within the first seconds after spawning.";
+        public string Command => Plugin.Instance.Config.CommandName;
+
+        public string[] Aliases => new[]
+        {
+            "rerollscp",
+            "scpreroll"
+        };
+
+        public string Description => "Reroll your SCP.";
 
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
@@ -25,13 +23,16 @@ namespace ScpReroll
 
             if (player == null)
             {
-                response = "Player not found.";
+                response = "Only players can use this command.";
                 return false;
             }
 
             bool success = Plugin.Instance.RerollManager.TryReroll(player);
 
-            response = success ? "Rolling..." : "Could not reroll.";
+            response = success
+                ? "Rerolling..."
+                : "You cannot reroll.";
+
             return success;
         }
     }
